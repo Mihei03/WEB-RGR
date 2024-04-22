b12d7ca74f18819b3655f1f06e65ec9b20bdbfb5
dunno <116356493+dunnotoru@users.noreply.github.com>
2024-04-22 18:06:02 +0700

    fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import './src/WeatherWidget.js';
+import './src/WeatherWidgetWeatherAPI.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-    import('./WeatherWidget.js').then(() => {
+    import('./WeatherWidgetWeatherAPI.js').then(() => {
       const widgetHTML = document.querySelector('#weather-widget').outerHTML;
       document.getElementById('app').innerHTML = widgetHTML;
     });
